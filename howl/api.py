6a128292c57e0a83f9e9c7430ff725d07f3d9ca6
orange <clinlee.me@gmail.com>
2016-11-13 22:34:11 +0800

    fix cros

--- howl/api.py
+++ howl/api.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request, redirect
 from flask_restful import Resource, Api, reqparse
+from flask_restful.utils import cors
 from config import es, whatwebdb
 from tasks import *
 
@@ -10,6 +11,7 @@ howlapi = Api(app)
 
 
 class SubdomainsList(Resource):
+    @cors.crossdomain(allow_origin='*')
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('limit', type=int, help='limit必须为int',default=1)
@@ -26,6 +28,7 @@ class SubdomainsList(Resource):
 
 
 class HowlList(Resource):
+    @cors.crossdomain(allow_origin='*')
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('limit', type=int, help='limit必须为int',default=100)
@@ -39,7 +42,7 @@ class HowlList(Resource):
             return {'count': len(hits), 'data': hits}
         else:
             return []
-
+    @cors.crossdomain(allow_origin='*')
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('netmask', type=int, help='netmask必须为int',default=24)
